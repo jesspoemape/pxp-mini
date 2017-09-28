10d6b93ab413899b7da6959e1de6e49ee692b1fd
Jess Poemape <jesspoemape@gmail.com>
2017-09-27 18:37:23 -0600

    articles in articled detail desktop styling done

--- src/components/ArticleDetail/ArticleTile.js
+++ src/components/ArticleDetail/ArticleTile.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import filler1 from './../../assets/filler1.jpg';
+
 const ArticleTile = () => {
     return (
         <Main>
+            <Filter />
             <Title>Article Title</Title>
             <Blurb>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et doloremagna aliqua.</Blurb>
             <ReadMoreButton>Read More</ReadMoreButton>
@@ -15,24 +18,40 @@ export default ArticleTile;
 
 const Main = styled.div`
     display: flex;
+    position relative;
     flex-direction: column;
     padding: 40px 60px;
+    background: url('${filler1}');
+    background-size: cover;
+`
+const Filter = styled.div`
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    z-index: 0;
+    position: absolute;
+    background-color: white;
+    opacity: 0.8;
+    filter: alpha(opacity=80);
 `
 const Title = styled.div`
     text-transform: uppercase;
-    color: #995599;
+    color: #7A237A;
     font-size: 30px;
     font-weight: 100;
     margin-bottom: 25px;
     text-align: left;
+    z-index: 1;
 `
 const Blurb = styled.div`
     text-align: left;
-    color: #B0B2B3;
+    color: #B3B3B3;
     font-size: 16px;
     line-height: 26px;
     font-weight: 100;
     margin-bottom: 40px;
+    z-index: 1;
 `
 const ReadMoreButton = styled.div`
     width: 75px;
@@ -44,6 +63,7 @@ const ReadMoreButton = styled.div`
     font-size: 9px;
     margin-bottom: 15px;
     color: #995599;
+    z-index: 1;
 
     border: 3px solid transparent;
     -moz-border-image: -moz-linear-gradient(left, #4C1683 0%, #BE10B7 100%);
